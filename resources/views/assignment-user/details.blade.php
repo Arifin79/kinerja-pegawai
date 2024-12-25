2e70a81f9fdeda51d703347ecf2322a9ee383a98
windini <aquariuswin1424@gmail.com>
2024-12-25 17:50:25 +0700

    Azizah - perbaikan kode badsmell pada views assignment-user details

--- resources/views/assignment-user/details.blade.php
+++ resources/views/assignment-user/details.blade.php
@@ -7,7 +7,10 @@
         <h1>Detail Tugas: {{ $task->title }}</h1>
         <p><strong>Nama Karyawan:</strong> {{ $task->employee->name }}</p>
         <p><strong>Tanggal:</strong> {{ $task->date }}</p>
-        <p><strong>Deskripsi:</strong> {{ $task->description }}</p>  <!-- Bisa ditambah deskripsi tugas -->
-        <p><strong>Gambar:</strong> <img src="{{ asset('images/' . $task->image) }}" alt="Task Image" /></p>
+        <p><strong>Deskripsi:</strong> {{ $task->description }}</p>
+        <p>
+            <strong>Gambar:</strong>
+            <img src="{{ asset('images/' . $task->image) }}" alt="{{ $task->title }}" />
+        </p>
     </div>
 @endsection
