aa1a6b8b82fbaa1d35b82e18ca6a9b90c675ce2e
windini <aquariuswin1424@gmail.com>
2024-12-27 01:32:49 +0700

    Azizah - Perbaikan kode badsmell pada views partials scripts

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,4 +1,3 @@
-{{-- Bootstrap 5.2 JS --}}
 <script src="{{ asset('bootstrap5/js/bootstrap.bundle.min.js') }}"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
@@ -9,7 +8,8 @@
     feather.replace({ 'aria-hidden': 'true' })
 </script>
 
-{{-- Main JS --}}
+
 <script type="module" src="{{ asset('js/main.js') }}"></script>
-{{-- Livewire JS --}}
+
 @livewireScripts
+
