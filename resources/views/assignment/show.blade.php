8161dd3234930a406e764f037c73b1fbcfcf033e
windini <aquariuswin1424@gmail.com>
2024-12-26 23:43:36 +0700

    Perbaikan kode badsmell views pada assignment show

--- resources/views/assignment/show.blade.php
+++ resources/views/assignment/show.blade.php
@@ -9,7 +9,7 @@
         <p><strong>Customer Name:</strong> {{ $assignment->customer_name }}</p>
         <p><strong>Employee Name:</strong> {{ $assignment->employee_name }}</p>
         <p><strong>Deadline:</strong> {{ $assignment->deadline }}</p>
-        <p><strong>Image:</strong> <img src="{{ asset('images/' . $assignment->image) }}" alt="Project Image" width="200" /></p>
+        <p><strong>Image:</strong> <img src="{{ asset('images/' . $assignment->image) }}" alt="Visual of {{ $assignment->project_name }}" width="200" /></p>
         <a href="{{ route('assignment.index') }}">Back to Assignments</a>
     </div>
 @endsection
